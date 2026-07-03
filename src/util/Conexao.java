1551d7d48adcc4d96cc18af63e6c17a75b967bd9
João Vitor Lêda Nogueira <leda@unifesspa.edu.br>
2026-07-03 10:51:37 -0300

    Consertando BD.sql e Conexao

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/BancoDeDados";
     private static final String USUARIO = "root";
+    private static final String SENHA = "Leda123";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL,USUARIO,SENHA);
